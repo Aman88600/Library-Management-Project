31b676bc37ab718400bfcd864f279d690ffe3266
Aman88600 <109413130+Aman88600@users.noreply.github.com>
2023-11-10 16:04:49 +0530

    2nd commit for 1st version

--- Project_v_1.c
+++ Project_v_1.c
@@ -6,27 +6,31 @@ int main(void)
 {
     // Getting user choice on entering books
     int response = 0;
-    printf("Press 1 to enter books\n");
-    printf("Press 0 to exit\n");
-    printf("Your response : ");
-    scanf("%i", &response);
 
-    if (response == 1)
+    do
     {
-        while ((getchar()) != '\n')
-            ;
-        char buffer[101];
-        printf("Enter your book name: ");
-        // Stores the name into auxiliary memory
-        scanf(" %100[^\n]", buffer);
-        // Creates a dynamic string
-        char *name = (char *)malloc(strlen(buffer) + 1);
-        // Sets the value
-        strcpy(name, buffer);
-        printf("Your name is %s\n", name);
-        // Frees the memory
-        free(name);
-        printf("Your name is %s", name);
-    }
+        printf("Press 1 to enter books\n");
+        printf("Press 0 to exit\n");
+        printf("Your response : ");
+        scanf("%i", &response);
+
+        if (response == 1)
+        {
+            while ((getchar()) != '\n')
+                ;
+            char buffer[101];
+            printf("Enter your book name: ");
+            // Stores the name into auxiliary memory
+            scanf(" %100[^\n]", buffer);
+            // Creates a dynamic string
+            char *name = (char *)malloc(strlen(buffer) + 1);
+            // Sets the value
+            strcpy(name, buffer);
+            printf("Your name is %s\n", name);
+            // Frees the memory
+            free(name);
+            printf("Your name is %s", name);
+        }
+    } while (response == 1);
     return 0;
 }
