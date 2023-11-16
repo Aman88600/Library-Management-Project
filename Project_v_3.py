9267df237ee2e1876c714c40f0ce310ec87b2f5a
Aman88600 <109413130+Aman88600@users.noreply.github.com>
2023-11-16 12:45:41 +0530

    Adding the add book feature to the console app

--- Project_v_3.py
+++ Project_v_3.py
@@ -4,20 +4,25 @@ try:
     con = sqlite3.connect("Library_Management.db")
     cur = con.cursor()
 
-    book_name = input("Enter Book name: ")
-    book_quantity = int(input("Enter Book quantity: "))
+    # Creating option to add book
+    user_choice = int(input("""
+    Enter 1 to Add book 
+    Enter 0 to exit : """))
 
-    cur.execute("INSERT INTO Books (Name, Quantity) VALUES (?, ?)", (book_name, book_quantity))
-
-    res = cur.execute("SELECT * FROM Books")
-    print(res.fetchall())
-
-    con.commit()  # Commit changes to the database
-
+    if (user_choice == 1):
+        book_name = input("Enter Book name: ")
+        book_quantity = int(input("Enter Book quantity: "))
+        cur.execute("INSERT INTO Books (Name, Quantity) VALUES (?, ?)", (book_name, book_quantity))
+        con.commit()  # Commit changes to the database
+        res = cur.execute("SELECT * FROM Books")
+        print(res.fetchall())
+    elif (user_choice == 0):
+        pass
 except sqlite3.Error as e:
     print("SQLite error:", e)
 
 finally:
     con.close()
 
-input("")
+# To hold the screen
+input("Press Enter to exit")
