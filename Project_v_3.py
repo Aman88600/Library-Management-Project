4c048ce822a6baac24e4841158f41dba0f27b059
Aman88600 <109413130+Aman88600@users.noreply.github.com>
2023-11-16 14:37:01 +0530

    Added the delete book feature

--- Project_v_3.py
+++ Project_v_3.py
@@ -7,12 +7,20 @@ try:
     # Creating option to add book
     user_choice = int(input("""
     Enter 1 to Add book 
+    Enter 2 to delete book
     Enter 0 to exit : """))
 
     if (user_choice == 1):
         book_name = input("Enter Book name: ")
         book_quantity = int(input("Enter Book quantity: "))
         cur.execute("INSERT INTO Books (Name, Quantity) VALUES (?, ?)", (book_name, book_quantity))
+        con.commit()  # Commit changes to the database
+        res = cur.execute("SELECT * FROM Books")
+        print(res.fetchall())
+
+    elif(user_choice == 2):
+        book_name = str(input("Enter Book name: "))
+        cur.execute(f"DELETE FROM Books WHERE Name = '{book_name}'")
         con.commit()  # Commit changes to the database
         res = cur.execute("SELECT * FROM Books")
         print(res.fetchall())
