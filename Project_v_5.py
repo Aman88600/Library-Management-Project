ae7e9d83f845a13d61118cc1dbefaa75b1e6a4fa
Aman88600 <109413130+Aman88600@users.noreply.github.com>
2023-11-19 11:13:40 +0530

    GUI With button still no db

--- Project_v_5.py
+++ Project_v_5.py
@@ -1,26 +1,73 @@
-# A GUI App for Library management 
 from tkinter import *
 
 root = Tk()
+root.geometry("500x500")
 
-# Setting the title
-root.title("Libaray")
-# Setting the initial size
-root.geometry("200x200")
-# root.iconbitmap('icons8-library-50.png')
-# Creating Books button
-Books = Button(root, text="Books", padx=50)
-# Putting book button on the screen
-Books.grid(row=0, column=0)
+# Specify Grid
+Grid.rowconfigure(root, 0, weight=1)
+Grid.columnconfigure(root, 0, weight=1)
 
-# Creating Members Button
-Members = Button(root, text="Members", padx=50)
-# Putting button on the screen
-Members.grid(row=1, column=0)
+Grid.rowconfigure(root, 1, weight=1)
+Grid.rowconfigure(root, 2, weight=1)
 
-# Creating Records Button 
-Records = Button(root, text = "Records", padx=50)
-# Putting Records button on the screen
-Records.grid(row=2, column=0)
+# Functions
+# Books
+def Books_function():
+    # Destroy the original buttons
+    Books.destroy()
+    Members.destroy()
+    Records.destroy()
 
+    # Create new Buttons
+    # See Books
+    See_Books = Button(root, text="See Available Books", command=see_books_function)
+    See_Books.grid(row=0, column=0, sticky="NSEW")
+
+    # Add Books
+    Add_Book = Button(root, text="Add Book")
+    Add_Book.grid(row=1, column=0, sticky="NSEW")
+
+    # Delete Book
+    Delete_Book = Button(root,text="Delete Book")
+    Delete_Book.grid(row=2, column=0, sticky="NSEW")
+
+# Members
+def Members_function():
+    # Destroy the original buttons
+    Books.destroy()
+    Members.destroy()
+    Records.destroy()
+
+    # Create new Buttons
+    # See Members
+    See_Members = Button(root, text="See Current Members")
+    See_Members.grid(row=0, column=0, sticky="NSEW")
+
+    # Add Members
+    Add_Member = Button(root, text="Add Member")
+    Add_Member.grid(row=1, column=0, sticky="NSEW")
+
+    # DeleteMembers
+    Delete_Member = Button(root,text="Delete Member")
+    Delete_Member.grid(row=2, column=0, sticky="NSEW")
+
+# See Books function
+def see_books_function():
+    # Create a new window or perform any other functionality
+    new_window = Toplevel(root)
+    new_window.geometry("300x200")
+    label = Label(new_window, text="List of Available Books")
+    label.pack()
+
+# Create Buttons
+Books = Button(root, text="Books", command=Books_function)
+Members = Button(root, text="Members", command=Members_function)
+Records = Button(root, text="Records")
+
+# Set grid
+Books.grid(row=0, column=0, sticky="NSEW")
+Members.grid(row=1, column=0, sticky="NSEW")
+Records.grid(row=2, column=0, sticky="NSEW")
+
+# Execute tkinter
 root.mainloop()
