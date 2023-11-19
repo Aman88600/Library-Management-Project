b096c8866ac3e1b3fbbce81f3c5a013f38fdd297
Aman88600 <109413130+Aman88600@users.noreply.github.com>
2023-11-19 11:24:19 +0530

    New Proposed model for gui

--- Project_v_5.py
+++ Project_v_5.py
@@ -1,5 +1,44 @@
 from tkinter import *
 
+def create_books_window():
+    books_window = Toplevel(root)
+    books_window.geometry("500x500")
+
+    # Specify Grid for the new window
+    Grid.rowconfigure(books_window, 0, weight=1)
+    Grid.columnconfigure(books_window, 0, weight=1)
+
+    Grid.rowconfigure(books_window, 1, weight=1)
+    Grid.rowconfigure(books_window, 2, weight=1)
+
+    # Create new Buttons on the new window
+    See_Books = Button(books_window, text="See Available Books")
+    See_Books.grid(row=0, column=0, sticky="NSEW")
+
+    Add_Book = Button(books_window, text="Add Book")
+    Add_Book.grid(row=1, column=0, sticky="NSEW")
+
+    Delete_Book = Button(books_window, text="Delete Book")
+    Delete_Book.grid(row=2, column=0, sticky="NSEW")
+
+def create_members_window():
+    members_window = Toplevel(root)
+    members_window.geometry("500x500")
+
+    # Specify Grid for the new window
+    Grid.rowconfigure(members_window, 0, weight=1)
+    Grid.columnconfigure(members_window, 0, weight=1)
+
+    # Create new Buttons on the new window
+    See_Members = Button(members_window, text="See Current Members")
+    See_Members.grid(row=0, column=0, sticky="NSEW")
+
+    Add_Member = Button(members_window, text="Add Member")
+    Add_Member.grid(row=1, column=0, sticky="NSEW")
+
+    Delete_Member = Button(members_window, text="Delete Member")
+    Delete_Member.grid(row=2, column=0, sticky="NSEW")
+
 root = Tk()
 root.geometry("500x500")
 
@@ -10,58 +49,9 @@ Grid.columnconfigure(root, 0, weight=1)
 Grid.rowconfigure(root, 1, weight=1)
 Grid.rowconfigure(root, 2, weight=1)
 
-# Functions
-# Books
-def Books_function():
-    # Destroy the original buttons
-    Books.destroy()
-    Members.destroy()
-    Records.destroy()
-
-    # Create new Buttons
-    # See Books
-    See_Books = Button(root, text="See Available Books", command=see_books_function)
-    See_Books.grid(row=0, column=0, sticky="NSEW")
-
-    # Add Books
-    Add_Book = Button(root, text="Add Book")
-    Add_Book.grid(row=1, column=0, sticky="NSEW")
-
-    # Delete Book
-    Delete_Book = Button(root,text="Delete Book")
-    Delete_Book.grid(row=2, column=0, sticky="NSEW")
-
-# Members
-def Members_function():
-    # Destroy the original buttons
-    Books.destroy()
-    Members.destroy()
-    Records.destroy()
-
-    # Create new Buttons
-    # See Members
-    See_Members = Button(root, text="See Current Members")
-    See_Members.grid(row=0, column=0, sticky="NSEW")
-
-    # Add Members
-    Add_Member = Button(root, text="Add Member")
-    Add_Member.grid(row=1, column=0, sticky="NSEW")
-
-    # DeleteMembers
-    Delete_Member = Button(root,text="Delete Member")
-    Delete_Member.grid(row=2, column=0, sticky="NSEW")
-
-# See Books function
-def see_books_function():
-    # Create a new window or perform any other functionality
-    new_window = Toplevel(root)
-    new_window.geometry("300x200")
-    label = Label(new_window, text="List of Available Books")
-    label.pack()
-
 # Create Buttons
-Books = Button(root, text="Books", command=Books_function)
-Members = Button(root, text="Members", command=Members_function)
+Books = Button(root, text="Books", command=create_books_window)
+Members = Button(root, text="Members", command=create_members_window)
 Records = Button(root, text="Records")
 
 # Set grid
