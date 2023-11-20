a1b537b79b9a5b104affd5d43e5f02d5f7720ee8
Aman88600 <109413130+Aman88600@users.noreply.github.com>
2023-11-20 20:13:07 +0530

    Add some new features

--- Project_v_5.py
+++ Project_v_5.py
@@ -1,4 +1,44 @@
 from tkinter import *
+import sqlite3
+import datetime
+
+con = sqlite3.connect("Library_Management.db")
+cur = con.cursor()
+
+# Avaliable books
+def See_Avaliable_Books():
+    # Getting data
+    res = cur.execute("SELECT * FROM Books")
+    records_data = res.fetchall()
+
+    # Create a new window
+    See_Avaliable_Books_window = Toplevel(root)
+    See_Avaliable_Books_window.geometry("500x500")
+
+    # Specify Grid for the new window
+    Grid.rowconfigure(See_Avaliable_Books_window, 0, weight=1)
+
+    # Create labels for each record
+    for i, record in enumerate(records_data):
+        record_label = Label(See_Avaliable_Books_window, text=f"Record {i + 1}: {record}")
+        record_label.grid(row=i, column=0, sticky="W", pady=5, padx=10)
+
+        # Adjust the grid row configuration for each label
+        Grid.rowconfigure(See_Avaliable_Books_window, i, weight=1)
+
+    # Adjust column weight to allow the labels to expand horizontally
+    Grid.columnconfigure(See_Avaliable_Books_window, 0, weight=1)
+
+# See all members
+# def see_all_members():
+
+#     # Getting data  
+#     res = cur.execute("SELECT * FROM Members")
+#     records_data = res.fetchall()
+
+#     # Create a new window
+#     See_Avaliable_Member_window = Toplevel(root)
+#     See_Avaliable_Member_window.geometry("500x500")
 
 def create_books_window():
     books_window = Toplevel(root)
@@ -12,7 +52,7 @@ def create_books_window():
     Grid.rowconfigure(books_window, 2, weight=1)
 
     # Create new Buttons on the new window
-    See_Books = Button(books_window, text="See Available Books")
+    See_Books = Button(books_window, text="See Available Books", command=See_Avaliable_Books)
     See_Books.grid(row=0, column=0, sticky="NSEW")
 
     Add_Book = Button(books_window, text="Add Book")
@@ -29,6 +69,9 @@ def create_members_window():
     Grid.rowconfigure(members_window, 0, weight=1)
     Grid.columnconfigure(members_window, 0, weight=1)
 
+    Grid.rowconfigure(members_window, 1, weight=1)
+    Grid.rowconfigure(members_window, 2, weight=1)
+
     # Create new Buttons on the new window
     See_Members = Button(members_window, text="See Current Members")
     See_Members.grid(row=0, column=0, sticky="NSEW")
@@ -38,6 +81,33 @@ def create_members_window():
 
     Delete_Member = Button(members_window, text="Delete Member")
     Delete_Member.grid(row=2, column=0, sticky="NSEW")
+
+
+def records_function():
+    # Getting data
+    res = cur.execute("SELECT * FROM Records")
+    records_data = res.fetchall()
+
+    # Create a new window
+    records_window = Toplevel(root)
+    records_window.geometry("500x500")
+
+    # Specify Grid for the new window
+    Grid.rowconfigure(records_window, 0, weight=1)
+
+    # Create labels for each record
+    for i, record in enumerate(records_data):
+        record_label = Label(records_window, text=f"Record {i + 1}: {record}")
+        record_label.grid(row=i, column=0, sticky="W", pady=5, padx=10)
+
+        # Adjust the grid row configuration for each label
+        Grid.rowconfigure(records_window, i, weight=1)
+
+    # Adjust column weight to allow the labels to expand horizontally
+    Grid.columnconfigure(records_window, 0, weight=1)
+
+
+
 
 root = Tk()
 root.geometry("500x500")
@@ -52,7 +122,7 @@ Grid.rowconfigure(root, 2, weight=1)
 # Create Buttons
 Books = Button(root, text="Books", command=create_books_window)
 Members = Button(root, text="Members", command=create_members_window)
-Records = Button(root, text="Records")
+Records = Button(root, text="Records", command=records_function)
 
 # Set grid
 Books.grid(row=0, column=0, sticky="NSEW")
