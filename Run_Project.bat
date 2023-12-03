f62759907168a8f25fe9f1743fced9b550c94c8f
Aman88600 <109413130+Aman88600@users.noreply.github.com>
2023-12-03 12:29:39 +0530

    Adding data base maker

---
+++ Run_Project.bat
@@ -0,0 +1,2 @@
+python data_base_maker.py
+python Project_v_6.py
---
+++ data_base_maker.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+# Connect to the SQLite database
+conn = sqlite3.connect('Library_Management.db')
+cursor = conn.cursor()
+
+# Define SQL statements to drop tables if they exist
+drop_books_table = "DROP TABLE IF EXISTS Books;"
+drop_members_table = "DROP TABLE IF EXISTS Members;"
+drop_records_table = "DROP TABLE IF EXISTS Records;"
+
+# Execute the SQL statements to drop tables
+cursor.execute(drop_books_table)
+cursor.execute(drop_members_table)
+cursor.execute(drop_records_table)
+
+# Define SQL statements to create tables
+create_books_table = """
+CREATE TABLE Books (
+    Name varchar(30),
+    Quantity int,
+    Issued int
+);
+"""
+
+create_members_table = """
+CREATE TABLE Members (
+    Name varchar(30)
+);
+"""
+
+create_records_table = """
+CREATE TABLE Records (
+    Book_Name varchar(30),
+    Member_Name varchar(30),
+    Issue_time timestamp,
+    Action varchar(20)
+);
+"""
+
+# Execute the SQL statements to create tables
+cursor.execute(create_books_table)
+cursor.execute(create_members_table)
+cursor.execute(create_records_table)
+
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
+
+print("Tables deleted and recreated successfully.")
